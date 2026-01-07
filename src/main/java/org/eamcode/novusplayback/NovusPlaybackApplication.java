cd024219674d189de44dad293f6538d5fc47eb11
emile <emuller2607@gmail.com>
2026-01-07 17:24:58 +0100

    Initial commit

---
+++ src/main/java/org/eamcode/novusplayback/NovusPlaybackApplication.java
@@ -0,0 +1,13 @@
+package org.eamcode.novusplayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovusPlaybackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovusPlaybackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eamcode/novusplayback/NovusPlaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eamcode.novusplayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovusPlaybackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
